8a42c7a146fd3b34449d887b416fc877b5385dbd
Vincent Wong <vincent@vwong.dev>
2020-05-15 21:40:37 +1000

    Updates

---
+++ precache-manifest.3444802b48f9ad4a7dba8fd7fc56c451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9ba62e9048c227d0398ae131d0f6d5",
+    "url": "/WebTetris/index.html"
+  },
+  {
+    "revision": "796266b3a713bc7da8e6",
+    "url": "/WebTetris/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e5830491a6c3e3faef63",
+    "url": "/WebTetris/static/css/main.62da18fb.chunk.css"
+  },
+  {
+    "revision": "796266b3a713bc7da8e6",
+    "url": "/WebTetris/static/js/2.55f89732.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/WebTetris/static/js/2.55f89732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5830491a6c3e3faef63",
+    "url": "/WebTetris/static/js/main.cd600cff.chunk.js"
+  },
+  {
+    "revision": "f5a8c2f590daf1cab664",
+    "url": "/WebTetris/static/js/runtime-main.d1b606ca.js"
+  }
+]);
