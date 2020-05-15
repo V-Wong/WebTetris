e0a592dbd9765e1cda192e9fbbe0bb5e98a9cf7f
Vincent Wong <vincent@vwong.dev>
2020-05-16 00:47:46 +1000

    Update navigation bar

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -8,9 +8,8 @@ export default class NavigationBar extends React.Component {
             <Navbar.Brand>Tetris Clone</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="about">About</Nav.Link>
-                <Nav.Link href="about">Source Code</Nav.Link>
-                <Nav.Link href="about">Other Projects</Nav.Link>
+                <Nav.Link href="https://github.com/V-Wong/WebTetris">Source Code</Nav.Link>
+                <Nav.Link href="https://vwong.dev">Other Projects</Nav.Link>
             </Nav>
         </Navbar>
         )
