48a538f5661d62704aaa65ecc3d88db98333f847
Vincent Wong <vincent@vwong.dev>
2020-05-15 21:32:40 +1000

    Update navigation bar

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -5,10 +5,12 @@ export default class NavigationBar extends React.Component {
     render() {
         return (
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand>Study Helper</Navbar.Brand>
+            <Navbar.Brand>Tetris Clone</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="about">About</Nav.Link>
+                <Nav.Link href="about">Source Code</Nav.Link>
+                <Nav.Link href="about">Other Projects</Nav.Link>
             </Nav>
         </Navbar>
         )
